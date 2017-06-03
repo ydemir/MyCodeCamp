564e99fa070fa2d99d9140306099161355c71cce
Yusuf Demir <yusuf@bilgiedin.net>
2017-06-03 04:23:46 +0300

    05 tamamlandı data model

---
+++ MyCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok(new { Name = "Yusuf", FavoriteColor = "Blue" });
+        }
+    }
+}
